a9013622fca8673fcc49722d97e7842d4ea304d0
Demonstr66 <Demonstr666@gmail.com>
2020-11-10 17:48:37 +0300

    Небольшой рефакторинг кода

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
 from tkinter import *
 import random
 from tkinter import messagebox as mb
-#!/usr/bin/env python
-# - coding: utf-8 -
 
 root = Tk()
 root.title("NextgenTetris")
-
-mainFrame = Frame( root, bd = 15) 
-mainFrame.pack( expand=True, fill='both' )
-
-menuFr = Frame( mainFrame, bd = 0)
-menuFr.pack( expand=True, fill='both' , side='top' )
-
-fieldFr = Frame( mainFrame, bd = 0)
-fieldFr.pack( expand=True, fill='both')
-
-colors = ["#f00", "#0f0", "#00f", "#ff0", "#f0f", "#0ff", "#000", "#fff"]
-
-
-rowscolumns = 3
-fieldColorsCode = []
-def fieldColorsCodefill():
-    for i in range(rowscolumns):
-        fieldColorsCode.append([])
-        for j in range(rowscolumns):
-            fieldColorsCode[i].append(0)
-fieldColorsCodefill()
-fieldOfBtns = []
-
 
 startX = IntVar(root)
 startY = IntVar(root)
@@ -39,25 +14,34 @@ startY.set(70)
 btnSize.set(80)
 
 rounds = 1
+rowscolumns = 3
+colors = ["#f00", "#0f0", "#00f", "#ff0", "#f0f", "#0ff", "#000", "#fff"]
+fieldModel = []
+fieldBtns = []
 
-showRounds = Label(menuFr,text=f'Rounds:{rounds}',font=('David',12),bg='#DDD')
-showRounds.pack(anchor='ne')
+def fieldModelfill():
+    for i in range(rowscolumns):
+        fieldModel.append([])
+        for j in range(rowscolumns):
+            fieldModel[i].append(0)
+fieldModelfill()
+
 
 def fieldClick(event):
     for i in range(rowscolumns):
         for j in range(rowscolumns):    
-            if event.widget == fieldOfBtns[i][j]:
-                currCode = fieldColorsCode[i][j]
+            if event.widget == fieldBtns[i][j]:
+                currCode = fieldModel[i][j]
                 currCode += 1                
                 if currCode == len(colors): currCode = 0
-                fieldColorsCode[i][j] = currCode
+                fieldModel[i][j] = currCode
                 event.widget.config( bg = colors[ currCode ] )
     check = True
-    sample = fieldColorsCode[0][0]
+    sample = fieldModel[0][0]
     for i in range(rowscolumns):
         
         for j in range(rowscolumns):
-            if  fieldColorsCode[i][j] != sample: check = False
+            if  fieldModel[i][j] != sample: check = False
     
     if check:
         global rounds
@@ -71,30 +55,30 @@ def makefield():
     startBtn.forget()
     levelsBtn.forget()
     for i in range(rowscolumns):
-        fieldOfBtns.append([])
+        fieldBtns.append([])
         fieldFr.rowconfigure(i, weight=1, minsize = btnSize.get())
         for j in range(rowscolumns):
             fieldFr.columnconfigure(j, weight=1,  minsize = btnSize.get())  
             
             colorCode = random.randint(0, len(colors) - 1)
-            fieldColorsCode[i][j] = colorCode
+            fieldModel[i][j] = colorCode
             btn = Button( fieldFr, bg = colors[ colorCode ])
             
             btn.grid( row=i, column=j, sticky='nsew')  
             
             btn.bind('<Button-1>',fieldClick)
-            fieldOfBtns[i].append( btn )
+            fieldBtns[i].append( btn )
 
 def newField():
     infoTxt.config( text = "WOW!!! YOU WIN!" )
     answ = mb.askokcancel('YOU WIN!','Start new round?')
     if answ:
         btnsDestroy()
-        fieldColorsCode.clear()
+        fieldModel.clear()
         global rowscolumns
         rowscolumns = 4
-        fieldColorsCodefill()
-        fieldOfBtns.clear()
+        fieldModelfill()
+        fieldBtns.clear()
         makefield()   
         showRounds['text'] = f'Rounds:{rounds}'
         infoTxt['text'] = 'All cells must be the same color'
@@ -102,14 +86,26 @@ def newField():
 def btnsDestroy():
     for i in range(rowscolumns):   
         for j in range(rowscolumns): 
-            fieldOfBtns[i][j].destroy()
+            fieldBtns[i][j].destroy()
         
 
+mainFrame = Frame( root, bd = 15) 
+mainFrame.pack( expand=True, fill='both' )
+
+menuFr = Frame( mainFrame, bd = 0)
+menuFr.pack( expand=True, fill='both' , side='top' )
+
+fieldFr = Frame( mainFrame, bd = 0)
+fieldFr.pack( expand=True, fill='both')
 
 startBtn = Button(menuFr,text='start',justify= CENTER,command = makefield,height=1,width=10)
 startBtn.pack()
+
 levelsBtn = Button(menuFr,text='levels',justify= CENTER,height=1,width=10)
 levelsBtn.pack()
+
+showRounds = Label(menuFr,text=f'Rounds:{rounds}',font=('David',12),bg='#DDD')
+showRounds.pack(anchor='ne')
 
 infoTxt = Label(
     menuFr,
