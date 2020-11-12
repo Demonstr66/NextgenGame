381e886a384d1abe432bf8fd7d31ed858ab14ac7
Demonstr66 <Demonstr666@gmail.com>
2020-11-12 19:19:17 +0300

    Added testEvent file

---
+++ eventTest.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+root.title("NextgenTetris")
+
+def show(e):
+    txt.pack()
+
+btn1 = Button(root, text="Создать событие")
+btn1.bind('<Button-1>', lambda e: root.event_generate('<<MyEvent>>'))
+btn1.pack()
+
+txt = Label(root, text="Событие получено")
+
+root.bind('<<MyEvent>>', show)
+
+
+
+root.mainloop()
